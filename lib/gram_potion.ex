7073c6a3f050cd37d676bb8ad1db9e69302a7f00
Putinspudding <35836135+Putinspudding@users.noreply.github.com>
2024-10-05 20:10:52 +0800

    mix init, test whether the workflow is woking

---
+++ lib/gram_potion.ex
@@ -0,0 +1,18 @@
+defmodule GramPotion do
+  @moduledoc """
+  Documentation for `GramPotion`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GramPotion.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
